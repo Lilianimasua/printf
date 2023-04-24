9062cd598fdd7bb608a9d0684d84b3901b065fa6
Lilianimasua <lilianimasua@gmailcom>
2023-04-24 13:45:01 -0700

    Corrected print numbers

--- specifier_handlers.c
+++ specifier_handlers.c
@@ -45,8 +45,25 @@ int print_digits(va_list, args_list)
 	int n, bytes_printed = 0;
 
 	n = va_arg(args_list, int);
+	
+	void print_number(int n)
 
-	write(1, &n, 1);
-	bytes_printed++;
-	return (bytes_printed);
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
